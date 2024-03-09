7080800a6ee4f1771e751da2d6b64c8fb3cd0966
Rafael Silva <ainbot6@gmail.com>
2024-03-09 15:57:04 -0300

    fix: Fix root.go command

--- cmd/root.go
+++ cmd/root.go
@@ -1,42 +1,51 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 
+*/
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+
+
+// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "wastrap",
-	Short: "-WordPress Security Scanner",
-	Long: `-Wastrap is a comprehensive security scanner for WordPress websites.
--It scans for vulnerabilities and provides information to improve the overall security of your WordPress installations.
--The CLI supports various commands to perform specific tasks.
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
 
--For more information, visit: https://github.com/institute-atri/wastrap`,
-	Run: func(cmd *cobra.Command, args []string) {
-		showWelcomeMessage()
-		_ = cmd.Help()
-	},
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-func showWelcomeMessage() {
-	fmt.Printf("\n🛡️ %s 🛡️\n", formatMessage("Welcome to Wastrap - WordPress Security Scanner", 1))
-	fmt.Println()
-}
-
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	err := rootCmd.Execute()
+	if err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.wastrap.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
+
+
