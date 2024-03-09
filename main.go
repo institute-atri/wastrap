f15ba8bae2a58b471d4876670dc69a39538e1fa1
Rafael Silva <ainbot6@gmail.com>
2024-03-09 19:14:12 -0300

    Resolved merge conflicts in main.go

--- main.go
+++ main.go
@@ -3,19 +3,11 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-<<<<<<< HEAD
 import (
 	"fmt"
 )
 
 func main() {
-	
+
 	fmt.Println("Hello world")
 }
-=======
-import "github.com/institute-atri/wastrap/cmd"
-
-func main() {
-	cmd.Execute()
-}
->>>>>>> origin/feat/#4
