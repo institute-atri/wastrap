01edc089681382ca15db63219def3b52911928d5
Rafael Silva <ainbot6@gmail.com>
2024-03-09 16:29:20 -0300

    main.go

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import "fmt"
+import "github.com/institute-atri/wastrap/cmd"
 
 func main() {
-	fmt.Println("Hello, Wastrap!")
+	cmd.Execute()
 }
