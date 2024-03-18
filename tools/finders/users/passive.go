551964aa05e1dfbe5c022dc3851296892bba8602
davidfontesfelix <davidfontes303@gmail.com>
2024-03-18 17:11:05 -0300

    feat:creating passive function

---
+++ tools/finders/users/passive.go
@@ -0,0 +1,30 @@
+package users
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/institute-atri/glogger"
+	"github.com/institute-atri/gnet"
+)
+
+func Passive(url string) {
+	urlFeed := url + "/feed"
+	
+	response := gnet.GET(urlFeed)
+
+	search := regexp.MustCompile(`<dc:creator><!\[CDATA\[(.+?)\]\]></dc:creator>`)
+
+	matches := search.FindAllStringSubmatch(response.BRaw, -1)
+
+	seenCreators := make(map[string]bool)
+
+	for _, match := range matches {
+		creators := strings.TrimPrefix(strings.TrimSuffix(match[1], "]]>"), "<![CDATA[")
+
+		if !seenCreators[creators] {
+			glogger.Done("Creator:", creators)
+			seenCreators[creators] = true
+		}
+	}
+}
